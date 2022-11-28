8ce8ea3441d625a4e42e562aecde6c02387e7137
AhmedAbdAlRahmanAnwar <ahmedabdalrahman61@gmail.com>
2022-11-29 00:35:25 +0200

    log file get end point

--- src/helpers/writeLogFile.js
+++ src/helpers/writeLogFile.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
+const path = require('path');
 
 module.exports = function writeTransactionLogs(transactionLog) {
-    fs.readFile('logs.json', function (err, data) {
+    fs.readFile(path.join(__dirname, '../../logs.json'), function (err, data) {
         const json = data.length ? JSON.parse(data) : [];
         json.push(transactionLog);
         fs.writeFile('logs.json', JSON.stringify(json), () => {});
