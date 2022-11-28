3eddab3592f35d5526a78f3a42f1a2b3b65cdb4d
AhmedAbdAlRahmanAnwar <ahmedabdalrahman61@gmail.com>
2022-11-28 18:51:47 +0200

    added reason code check

--- src/controllers/transactionResponse.controller.js
+++ src/controllers/transactionResponse.controller.js
@@ -1,8 +1,8 @@
 function handleTransaction(req, res) {
-    const {decision, req_reference_number, signed_date_time, transaction_id} = req.body;
+    const {decision, req_reference_number, signed_date_time, transaction_id, reason_code} = req.body;
     const queryString = `decision=${decision}&req_reference_number=${req_reference_number}&signed_date_time=${signed_date_time}&transaction_id=${transaction_id}`;
 
-    if (decision === 'ACCEPT') {
+    if (decision === 'ACCEPT' && reason_code === 100) {
         res.redirect(`/success?${queryString}`);
     } else {
         res.redirect(`/failure?${queryString}`);
