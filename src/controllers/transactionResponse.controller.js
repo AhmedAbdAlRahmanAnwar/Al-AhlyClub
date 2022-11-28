5a4db374d33e343e45714a33250919b486be2454
AhmedAbdAlRahmanAnwar <ahmedabdalrahman61@gmail.com>
2022-11-28 18:59:49 +0200

    add validation middleware

--- src/controllers/transactionResponse.controller.js
+++ src/controllers/transactionResponse.controller.js
@@ -1,8 +1,9 @@
 function handleTransaction(req, res) {
     const {decision, req_reference_number, signed_date_time, transaction_id, reason_code} = req.body;
     const queryString = `decision=${decision}&req_reference_number=${req_reference_number}&signed_date_time=${signed_date_time}&transaction_id=${transaction_id}`;
+    const SUCCESS_CODE = '100';
 
-    if (decision === 'ACCEPT' && reason_code === '100') {
+    if (decision === 'ACCEPT' && reason_code === SUCCESS_CODE) {
         res.redirect(`/success?${queryString}`);
     } else {
         res.redirect(`/failure?${queryString}`);
--- src/routes/transactionResponse.route.js
+++ src/routes/transactionResponse.route.js
@@ -7,7 +7,7 @@ const transactionResponseSchema = require('../dto/transactionResponse.dto')
 
 router.post('/handle-transaction',
     protect,
-    // requestValidator(transactionResponseSchema),
+    requestValidator(transactionResponseSchema),
     transactionResponseHandler);
 
 module.exports = router;
