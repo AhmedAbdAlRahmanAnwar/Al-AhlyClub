1a582a2443101c00c468bb934d98b8716c61e5aa
AhmedAbdAlRahmanAnwar <ahmedabdalrahman61@gmail.com>
2022-11-29 00:26:26 +0200

    log file get end point

--- src/routes/initializeAppRoutes.js
+++ src/routes/initializeAppRoutes.js
@@ -9,6 +9,7 @@ const checkoutRoute = require('./checkout.route');
 const handleTransactionRoute = require('./transactionResponse.route');
 const successRoute = require('../routes/success.route');
 const failureRoute = require('../routes/failure.route');
+const logFile = require('../../logs.json');
 
 // Register all app routes
 module.exports = (app) => {
@@ -18,6 +19,11 @@ module.exports = (app) => {
     app.use(`${baseUrl}`, handleTransactionRoute);
     app.use(successRoute);
     app.use(failureRoute);
+
+    // Send log file
+    app.get('/logs',(req, res)=>{
+        res.json(logFile);
+    })
 
     // Not found MiddleWare
     app.use(notFoundHandler);
