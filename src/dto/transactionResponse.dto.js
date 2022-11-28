adbd819d52358d3e56032543798d9bc594487d9f
AhmedAbdAlRahmanAnwar <ahmedabdalrahman61@gmail.com>
2022-11-28 18:55:15 +0200

    change reason code type check to string

--- src/dto/transactionResponse.dto.js
+++ src/dto/transactionResponse.dto.js
@@ -3,8 +3,7 @@ const Joi = require('joi');
 const transactionResponseSchema = {
     body: Joi.object().required().keys({
         decision: Joi.string().required().valid('ACCEPT', 'DECLINE', 'REVIEW', 'ERROR', 'CANCEL'),
-        req_reference_number: Joi.string().required(),
-        message: Joi.string().required()
+        req_reference_number: Joi.string().required()
     })
 }
 
