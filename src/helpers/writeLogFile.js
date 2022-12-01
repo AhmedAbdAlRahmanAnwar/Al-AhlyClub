c72f3243285a9920bc81920b62dc70a1d375466b
AhmedAbdAlRahmanAnwar <ahmedabdalrahman61@gmail.com>
2022-12-01 12:34:04 +0200

    test merchant callback

--- src/helpers/writeLogFile.js
+++ src/helpers/writeLogFile.js
@@ -6,8 +6,11 @@ module.exports = function writeTransactionLogs(transactionLog) {
     const logFile = decision === 'ACCEPT' ? 'successLogs.json' : 'failureLogs.json';
 
     fs.readFile(path.join(__dirname, `../logs/${logFile}`), function (err, data) {
-        const json = data.length ? JSON.parse(data) : [];
-        json.push(transactionLog);
-        fs.writeFile(path.join(__dirname, `../logs/${logFile}`), JSON.stringify(json), () => {});
+        console.log(err)
+        if (err === null){
+            const json = data.length ? JSON.parse(data) : [];
+            json.push(transactionLog);
+            fs.writeFile(path.join(__dirname, `../logs/${logFile}`), JSON.stringify(json), () => {});
+        }
     });
 }
