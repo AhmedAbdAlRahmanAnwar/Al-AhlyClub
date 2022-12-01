8b18d63bc91005b19122da7f3d9a5295e537d14d
AhmedAbdAlRahmanAnwar <ahmedabdalrahman61@gmail.com>
2022-12-01 11:34:16 +0200

    test origin

--- src/middlewares/verifyRequestOrigin.middleware.js
+++ src/middlewares/verifyRequestOrigin.middleware.js
@@ -1,9 +1,11 @@
 const {cyberSource} = require('../config');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
-module.exports = (req, res, next)=>{
+module.exports = (req, res, next) => {
+    console.log(req.headers)
+    console.log(req.header('origin'))
+    console.log(req.origin);
     console.log(req.get('origin'));
-    console.log(req.get('Origin'));
     if (req.get('origin') !== cyberSource.url) {
         throw new UnauthorizedError();
     }
