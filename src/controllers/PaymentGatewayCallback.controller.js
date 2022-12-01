e3237a6fb5c6399c6f7e4bcc9e0ec89cdafc1736
AhmedAbdAlRahmanAnwar <ahmedabdalrahman61@gmail.com>
2022-12-01 13:40:24 +0200

    fixed payment bug

--- src/controllers/PaymentGatewayCallback.controller.js
+++ src/controllers/PaymentGatewayCallback.controller.js
@@ -4,21 +4,19 @@ const callAlAhlyApi = require('../helpers/callAlAhlyApi');
 const writeTransactionLog = require("../helpers/writeLogFile");
 
 module.exports = async function handlePaymentGatewayCallback(req, res) {
-    // try {
+    try {
         const {decision, reason_code, req_reference_number, req_amount} = req.body;
         const SUCCESS_CODE = '100';
 
         writeTransactionLog(req.body);
-
         if (decision === 'ACCEPT' && reason_code === SUCCESS_CODE && !isPaymentTampered(req.body)) {
             const {data} = await callAlAhlyApi(req_reference_number, req_amount);
-            console.log("data xml alahly :" + data);
             data ? res.status(SUCCESS).send() : res.status(INTERNAL_ERROR).send();
             return;
         }
 
         res.status(BAD_REQUEST).send();
-    // } catch (error) {
-    //     res.status(BAD_REQUEST).send();
-    // }
+    } catch (error) {
+        res.status(BAD_REQUEST).send();
+    }
 }
--- src/routes/membershipPayment.route.js
+++ src/routes/membershipPayment.route.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const handlePaymentGatewayCallback = require('../controllers/PaymentGatewayCallback.controller');
 
-router.post('/merchantNotification',
-    handlePaymentGatewayCallback);
+router.post('/merchantNotification', handlePaymentGatewayCallback);
 
 module.exports = router;
