0e47fbac96dc1b4c0dfb0d4b9f609ef467db22c0
AhmedAbdAlRahmanAnwar <ahmedabdalrahman61@gmail.com>
2022-12-01 11:13:00 +0200

    trace origin url

--- src/middlewares/verifyRequestOrigin.middleware.js
+++ src/middlewares/verifyRequestOrigin.middleware.js
@@ -2,6 +2,8 @@ const {cyberSource} = require('../config');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next)=>{
+    console.log(req.get('origin'));
+    console.log(req.get('Origin'));
     if (req.get('origin') !== cyberSource.url) {
         throw new UnauthorizedError();
     }
