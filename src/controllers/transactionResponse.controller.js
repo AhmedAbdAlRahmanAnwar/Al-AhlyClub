af8fda72b27eb44a97d6775f5da6881f79fd76b9
Ahmed Anwar <24815246+AhmedAbdAlRahmanAnwar@users.noreply.github.com>
2022-12-06 15:14:28 +0200

    Add files via upload

--- src/controllers/transactionResponse.controller.js
+++ src/controllers/transactionResponse.controller.js
@@ -1,10 +1,11 @@
+const isPaymentTampered = require('../helpers/paymentValidationChecker');
 
 function handleTransaction(req, res) {
     const {decision, req_reference_number, signed_date_time, transaction_id, reason_code, req_amount} = req.body;
     const queryString = `decision=${decision}&req_reference_number=${req_reference_number}&signed_date_time=${signed_date_time}&transaction_id=${transaction_id}&req_amount=${req_amount}`;
     const SUCCESS_CODE = '100';
 
-    if (decision === 'ACCEPT' && reason_code === SUCCESS_CODE) {
+    if (decision === 'ACCEPT' && reason_code === SUCCESS_CODE && !isPaymentTampered(req.body)) {
         res.redirect(`/success?${queryString}`);
     } else {
         res.redirect(`/failure?${queryString}`);
